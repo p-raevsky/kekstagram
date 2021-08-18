5da4d5838857a2c5d9e48e41306933ce66c7d4fc
Павел <raevsky3@gmail.com>
2021-08-19 01:20:26 +0300

    Adds some fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   entry: './source/js/main.js',
